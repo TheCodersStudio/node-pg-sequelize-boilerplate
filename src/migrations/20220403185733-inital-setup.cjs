8c56dffd00f96346ef1038a6b67a995c4721bf35
Chetan Patil <chetan.patil2@ingka.ikea.com>
2022-08-14 22:24:44 +0200

    - fix: migration script updated - feat: swagger docs added for post users

--- src/migrations/20220403185733-inital-setup.cjs
+++ src/migrations/20220403185733-inital-setup.cjs
@@ -1,57 +1,39 @@
 const { Sequelize, DataTypes } = require('sequelize');
 
 const tableNames = ['user', 'skill'];
-const models = {
-  user: {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    gender: {
-      type: DataTypes.ENUM,
-      values: ['Male', 'Female', 'Other'],
-    },
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-  },
-  skill: {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    userId: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: 'user',
-        key: 'id',
-      },
-    },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    proficiency: {
-      type: DataTypes.ENUM,
-      values: ['Beginer', 'Intermediate', 'Advanced'],
-    },
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE,
-  }
-};
 
 module.exports = {
   up: async (queryInterface) => {
-    await queryInterface.sequelize.transaction(async (transaction) => {
-      await queryInterface.createTable('user', models.user, { transaction });
-      await queryInterface.createTable('skill', models.skill, { transaction });
-    });
+    await queryInterface.sequelize.query(`
+      CREATE TYPE enum_user_gender AS ENUM ('Male', 'Female', 'Other');
+      CREATE TYPE enum_skill_proficiency AS ENUM ('Beginer', 'Intermediate', 'Advanced');
+
+      CREATE TABLE IF NOT EXISTS public."user"
+      (
+          id serial NOT NULL,
+          first_name character varying(128) COLLATE pg_catalog."default",
+          last_name character varying(128) COLLATE pg_catalog."default",
+          gender enum_user_gender,
+          created_at timestamp with time zone,
+          updated_at timestamp with time zone,
+          CONSTRAINT user_pkey PRIMARY KEY (id)
+      );
+        
+      CREATE TABLE IF NOT EXISTS public.skill
+      (
+          id serial NOT NULL,
+          user_id integer NOT NULL,
+          name character varying(255) COLLATE pg_catalog."default" NOT NULL,
+          proficiency enum_skill_proficiency,
+          created_at timestamp with time zone,
+          updated_at timestamp with time zone,
+          CONSTRAINT skill_pkey PRIMARY KEY (id),
+          CONSTRAINT "skill_user_id_fkey" FOREIGN KEY (user_id)
+              REFERENCES public."user" (id) MATCH SIMPLE
+              ON UPDATE NO ACTION
+              ON DELETE NO ACTION
+      );
+    `);
   },
 
   down: async (queryInterface, Sequelize) => {
--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -8,15 +8,99 @@ const router = express.Router();
 const { validate } = new Validator();
 
 /**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Skills:
+ *       type: object
+ *       properties:
+ *         name:
+ *           type: string
+ *           description: Skill name.
+ *           example: JavaScript
+ *         proficiency:
+ *           type: string
+ *           description: Skill proficiency level.
+ *           enum: ['Beginer', 'Intermediate', 'Advanced']
+ *     User:
+ *       type: object
+ *       properties:
+ *         firstName:
+ *           type: string
+ *           description: The user's first/given name.
+ *           example: Chetan
+ *         lastName:
+ *           type: string
+ *           description: The user's surname/family name.
+ *           example: Patil
+ *         gender:
+ *           type: string
+ *           enum: [Male, Female, Other]
+ * 
+ *     CreateUserRequest:
+ *       allOf:
+ *       - $ref: '#/components/schemas/User'
+ *       - type: object
+ *         properties:
+ *           skills:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Skills'
+ * 
+ *     CreateUserSuccess:
+ *       type: object
+ *       properties:
+ *         success:
+ *           type: boolean
+ *           description: Flag stating status of API call
+ *           example: true
+ *         body:
+ *           allOf:
+ *           - $ref: '#/components/schemas/User'
+ *           - type: object
+ *             properties:
+ *               id:
+ *                 type: number
+ *                 description: Id generated for created user.
+ *                 example: 1
+ *               skills:
+ *                 type: array
+ *                 items:
+ *                   allOf:
+ *                   - type: object
+ *                     properties:
+ *                       id:
+ *                         type: number
+ *                         description: Id generated for created skill.
+ *                         example: 1
+ *                       userId:
+ *                         type: number
+ *                         description: Id generated for created user.
+ *                         example: 1
+ *                   - $ref: '#/components/schemas/Skills'
+ */
+
+/**
  * @openapi
  * /v1/users:
  *   post:
  *     tags:
  *       - v1
- *     description: Health Endpoint
+ *     description: Endpoint to create/add new user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateUserRequest'
  *     responses:
  *       200:
  *         description: Application helath details.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/CreateUserSuccess'
+ *  
  */
 router
   .route('/')
